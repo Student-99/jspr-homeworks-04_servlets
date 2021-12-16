3f70b665b6bd0c920384d5cb61d861cdfdb67b1b
Student <ekbstudent@yandex-team.ru>
2021-12-16 10:38:10 +0500

    Исправил замечания

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,92 +1,99 @@
-//package ru.netology.servlet;
-//
-//import java.io.IOException;
-//
-//import ru.netology.controller.PostController;
-//import ru.netology.repository.PostRepository;
-//import ru.netology.service.PostService;
-//
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.ServletContext;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@WebServlet(value = "/", loadOnStartup=1)
-//public class MainServlet extends HttpServlet {
-//    private PostController controller;
-//
-//    @Override
-//    public void init() {
-//        final var repository = new PostRepository();
-//        final var service = new PostService(repository);
-//        controller = new PostController(service);
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-//        final var path = req.getRequestURI();
-//
-//        if (path.equals("/api/get")||path.equals("/")) {
-//            controller.all(resp);
-//            return;
-//        }
-//        if (path.matches("/api/get/\\d+")) {
-//            // easy way
-//            final var id = getIdFromURL(path);
-//            controller.getById(id, resp);
-//            return;
-//        }
-//        super.doGet(req,resp);
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-//        final var path = req.getRequestURI();
-//
-//        if (path.equals("/api/posts")) {
-//            controller.save(req.getReader(), resp);
-//            return;
-//        }
-//        super.doPost(req,resp);
-//    }
-//
-//    @Override
-//    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-//        final var path = req.getRequestURI();
-//        if (path.matches("/api/delete/\\d+")) {
-//            final var id = getIdFromURL(path);
-//            controller.removeById(id, resp);
-//            return;
-//        }
-//        super.doDelete(req,resp);
-//    }
-//
-//    @Override
-//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-//        final var method = req.getMethod();
-//
-//        if (method.equals("GET")){
-//            doGet(req,resp);
-//            return;
-//        }
-//        if (method.equals("POST")){
-//            doPost(req,resp);
-//            return;
-//        }
-//        if (method.equals("DELETE")){
-//            doDelete(req,resp);
-//            return;
-//        }
-//
-//       resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//    }
-//
-//    private long getIdFromURL(String url){
-//       return Long.parseLong(url.substring(url.lastIndexOf("/")+1));
-//    }
-//}
-//
+package ru.netology.servlet;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/", loadOnStartup=1)
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+
+    @Override
+    public void init() {
+        // отдаём список пакетов, в которых нужно искать аннотированные классы
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+
+        // получаем по имени бина
+         controller = (PostController) context.getBean("postController");
+
+        // получаем по классу бина
+        final var service = context.getBean(PostService.class);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        final var path = req.getRequestURI();
+
+        if (path.equals("/api/get")||path.equals("/")) {
+            controller.all(resp);
+            return;
+        }
+        if (path.matches("/api/get/\\d+")) {
+            // easy way
+            final var id = getIdFromURL(path);
+            controller.getById(id, resp);
+            return;
+        }
+        super.doGet(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        final var path = req.getRequestURI();
+
+        if (path.equals("/api/posts")) {
+            controller.save(req.getReader(), resp);
+            return;
+        }
+        super.doPost(req,resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        final var path = req.getRequestURI();
+        if (path.matches("/api/delete/\\d+")) {
+            final var id = getIdFromURL(path);
+            controller.removeById(id, resp);
+            return;
+        }
+        super.doDelete(req,resp);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        final var method = req.getMethod();
+
+        if (method.equals("GET")){
+            doGet(req,resp);
+            return;
+        }
+        if (method.equals("POST")){
+            doPost(req,resp);
+            return;
+        }
+        if (method.equals("DELETE")){
+            doDelete(req,resp);
+            return;
+        }
+
+       resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    private long getIdFromURL(String url){
+       return Long.parseLong(url.substring(url.lastIndexOf("/")+1));
+    }
+}
+
